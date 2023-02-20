5267dc24e492630f6343ba512198f8dcd94e41d6
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-20 14:02:04 -0500

    Ejercicio Cuatro

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
 alert("Abrir la consola o presione F12")
 
-let num1 = Number(prompt("Ingrese el primer sumando"));
-let num2 = Number(prompt("Ingrese segundo sumando"));
+let variableA = Number(prompt("Ingrese un número"));
+let variableB = Number(prompt("Ingrese un segundo número"));
+let variableC = Number(prompt("Ingrese un tercer número"));
 
-alert (`${num1} + ${num2} = ${num1+num2}`)
+alert(`Entre los tres números ingresado el mayor es: ${Math.max(variableA,variableB,variableC)}`);
+alert(`Entre los tres números ingresado el minimo es: ${Math.min(variableA,variableB,variableC)}`);
