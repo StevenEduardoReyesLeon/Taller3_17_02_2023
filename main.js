1efc505ea106dc5db1a0c9facafbbd52fcb32a6a
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-20 15:43:21 -0500

    Ejercicio Seis

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 
 alert("Abrir la consola o presione F12")
 
-console.log(`100 primeros pares`);
+let num1 = 1;
+let num2 = 0;
+let num3 = 1;
+let continuar = true;
+let numActual;
 
-let num1 = 1
+let numPares = "Los números pares son: ";
+let numImpares = "Los números impares son: ";
 
-for(let i = 2; i <= 100; i += 2){
-    console.log(`${num1}. ${i}`);
-    num1 ++;
+while(continuar){
+    numActual = Number(prompt("Ingrese un número"));
+
+    if(numActual % 2 == 0){
+        num2 += numActual;
+        numPares += `${numActual} `
+    }else{
+        num3 = num3 * numActual;
+        numImpares += `${numActual} `
+    }
+    continuar = Number(prompt("Para continuar presione 1 o presione 0 para cancelar"));
 }
+
+alert(`${numPares} y su suma es = ${num2}`);
+alert(`${numImpares} y su multiplicación es = ${num3}`);
