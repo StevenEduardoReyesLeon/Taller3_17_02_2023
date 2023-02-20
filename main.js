73ae2f34a42043b739d9e33b93076b7beb95b9eb
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-20 17:39:11 -0500

    Ejercicio Siete

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
 
 alert("Abrir la consola o presione F12")
 
-let num1 = 1;
-let num2 = 0;
-let num3 = 1;
-let continuar = true;
-let numActual;
-
-let numPares = "Los números pares son: ";
-let numImpares = "Los números impares son: ";
-
-while(continuar){
-    numActual = Number(prompt("Ingrese un número"));
-
-    if(numActual % 2 == 0){
-        num2 += numActual;
-        numPares += `${numActual} `
-    }else{
-        num3 = num3 * numActual;
-        numImpares += `${numActual} `
+let num= Number(prompt("Ingrese un número"));
+let suma=0;
+    for (let i = 1; i < num; i++) 
+    {
+        if (num%i==0) 
+        {
+         suma +=i;   
+        }
     }
-    continuar = Number(prompt("Para continuar presione 1 o presione 0 para cancelar"));
-}
+    (suma==num)?alert("Es un numero perfecto"):alert("No es un numero perfecto");
 
-alert(`${numPares} y su suma es = ${num2}`);
-alert(`${numImpares} y su multiplicación es = ${num3}`);
