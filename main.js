69d693f3c93e874d0c8c7d6dc7e0611ba10ca965
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-20 14:08:46 -0500

    Ejercicio Cinco

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
 alert("Abrir la consola o presione F12")
 
-let variableA = Number(prompt("Ingrese un número"));
-let variableB = Number(prompt("Ingrese un segundo número"));
-let variableC = Number(prompt("Ingrese un tercer número"));
+console.log(`100 primeros pares`);
 
-alert(`Entre los tres números ingresado el mayor es: ${Math.max(variableA,variableB,variableC)}`);
-alert(`Entre los tres números ingresado el minimo es: ${Math.min(variableA,variableB,variableC)}`);
+let num1 = 1
+
+for(let i = 2; i <= 100; i += 2){
+    console.log(`${num1}. ${i}`);
+    num1 ++;
+}
